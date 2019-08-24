b2c2fc2bbff6bbb9b8326d5dc459e321b2d9ec3a
tswsxk <tongsw@mail.ustc.edu.cn>
2019-08-24 15:25:21 +0800

    测试非截断

--- XKT/shared/etl.py
+++ XKT/shared/etl.py
@@ -10,11 +10,13 @@ __all__ = ["extract"]
 
 def extract(data_src):
     responses = []
+    step = 200
     with open(data_src) as f:
         for line in tqdm(f, "reading data from %s" % data_src):
-            data = json.loads(line)[:200]
-            if len(data) < 2:
-                continue
-            responses.append(data)
+            data = json.loads(line)
+            for i in range(0, len(data), step):
+                if len(data[i: i + step]) < 2:
+                    continue
+                responses.append(data[i: i + step])
 
     return responses

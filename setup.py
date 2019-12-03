b58d576b478f33a1accda92998bf11d9a44ef00e
tswsxk <tongsw@mail.ustc.edu.cn>
2019-12-03 19:14:26 +0800

    test for XKT

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 # coding: utf-8
 # create by tongshiwei on 2019/6/25
 
-from setuptools import setup
+from setuptools import setup, find_packages
+
+test_deps = [
+    'pytest>=4',
+    'pytest-cov>=2.6.0',
+    'pytest-pep8>=1',
+    'EduData',
+]
 
 setup(
     name='XKT',
     version='0.0.1',
+    packages=find_packages(),
+    python_requires='>=3.6',
+    long_description='Refer to full documentation https://github.com/bigdata-ustc/XKT/blob/master/README.md'
+                     ' for detailed information.',
+    description='This project aims to '
+                'provide convenient interfaces for downloading and preprocessing dataset in education.',
+    extras_require={
+        'test': test_deps,
+    },
     install_requires=[
         'tqdm',
         'mxnet',
---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# 2019/12/3 @ tongshiwei
+
+import pytest
+from EduData import get_data
+
+
+
